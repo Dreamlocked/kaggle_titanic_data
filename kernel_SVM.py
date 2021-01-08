eec437b136d77b55f19c93bd87a39e72c5b3db82
Leonardo Olaechea <leonardo.olae.saav@gmail.com>
2021-01-08 00:07:41 -0500

    Subiendo los resultados -->0.776 Y comparé el SVM y XGBoost --> SVM ganó, tendré que avergiuar más sobre eso

--- kernel_SVM.py
+++ kernel_SVM.py
@@ -11,7 +11,6 @@ import pandas as pd
 
 # Importar el dataset
 dataset = pd.read_csv('train.csv')
-
 # Eliminando NaN
 # dataset[11] = dataset[11].replace(np.nan,0)
 dataset['Cabin'] = dataset['Cabin'].replace(np.nan, 0)
@@ -94,3 +93,60 @@ cm_two = confusion_matrix(y_test, y_pred_two)
 accuracies_two = cross_val_score(estimator = classifier_two, X = X_train, y = y_train, cv = 20)
 accuracies_two.mean()
 accuracies_two.std()
+
+##########################
+# Ahora, para el nuevo dataset
+dataset_test = pd.read_csv('test.csv')
+# Eliminando NaN
+# dataset[11] = dataset[11].replace(np.nan,0)
+dataset_test['Cabin'] = dataset_test['Cabin'].replace(np.nan, 0)
+for i in range(0,len(dataset_test['Cabin'])):
+    if type(dataset_test['Cabin'][i]) is str:
+        dataset_test['Cabin'][i] = 1
+dataset_test['Embarked'] = dataset_test['Embarked'].replace(np.nan, "S")
+
+X_t = dataset_test.iloc[:, [1,3,4,5,6,8,9,10]].values
+
+labelencoder_X = LabelEncoder()
+X_t[:, 1] = labelencoder_X.fit_transform(X_t[:, 1])
+
+# Codificar datos categoricos boleto
+
+X_t = np.array(ct.transform(X_t))
+# Forma bruta de eliminar una columna, aunque no se elimina de verdad
+X_t = X_t[:,1:]
+
+# Codificar datos de embarque
+labelencoder_X = LabelEncoder()
+X_t[:, 8] = labelencoder_X.fit_transform(X_t[:, 8])
+
+X_t = np.array(ct_1.fit_transform(X_t))
+X_t = X_t[:,1:]
+
+X_t= X_t.astype(np.float)
+imputed = mice(X_t)
+mice_ages_two = imputed[:, 5]
+X_t[:,5] = mice_ages_two
+
+imputed = mice(X_t)
+idk = imputed[:, 8]
+X_t[:,8] = idk
+
+X_t = sc_X.transform(X_t)
+y_pred_a = classifier.predict(X_t) 
+y_pred_sub  = classifier_two.predict(X_t)
+
+#Submmit
+dataset_res = pd.read_csv('gender_submission.csv')
+y_sub = dataset_res.iloc[:, 1].values
+cm_tre = confusion_matrix(y_sub , y_pred_a)
+cm_for = confusion_matrix(y_sub , y_pred_sub)
+accuracies_three = cross_val_score(estimator = classifier, X = X_t, y = y_sub, cv = 20)
+accuracies_for = cross_val_score(estimator = classifier_two, X = X_t, y = y_sub, cv = 20)
+
+#Submmit
+x_id = dataset_test.iloc[:, 0].values
+data = {"PassengerId":x_id, "Survived":y_pred_a}
+df = pd.DataFrame(data, columns = ["PassengerId", "Survived"])
+df.to_csv('submmit.csv', index = False)
+prueba = pd.read_csv('submmit.csv')
